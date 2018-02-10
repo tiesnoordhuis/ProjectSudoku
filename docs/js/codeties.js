a82047c7512d600e9bd489162827c8d216c4410c
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-02-10 21:09:29 +0100

    added stylesheet en debug window

--- docs/js/codeties.js
+++ docs/js/codeties.js
@@ -12,3 +12,15 @@ function checkCode() {
     document.getElementById("titleDiv").innerHTML = "succes!";
   }
 }
+
+window.onload = frameIni();
+
+function frameIni() {
+  var width = screen.width;
+  var height = screen.height;
+}
+
+function showDebug() {
+  document.getElementById("debugDiv").innerHTML = "De width is " + screen.width;
+  document.getElementById("debugDiv").innerHTML += "De height is " + screen.height;
+}
