27fb7b817464707d795e3c1bf09e43b479c9be73
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-02-18 01:58:36 +0100

    setup extra
    funcites voor boordcomputer toegevoegd
    plaatjes achtergrond
    positioning
    proef versie rdy

--- docs/js/codeties.js
+++ docs/js/codeties.js
@@ -5,11 +5,28 @@ comments
 geschreven door Ties Noordhuis
 
 */
-
 function checkCode() {
   var x = document.getElementById("codeInput").value;
   if (x == "18") {
     document.getElementById("titleDiv").innerHTML = "succes!";
+    hide(1);
+    document.getElementById("body").style.backgroundImage = "url('images/boordcomputer2.jpg')";
+    setTimeout(newScreen, 30000);
+  } else {
+    document.getElementById("titleDiv").innerHTML = "FOUT!";
+    document.getElementById("codeInput").value = "";
+    document.getElementById("body").style.backgroundImage = "url('images/boordcomputer3.jpg')";
+  }
+}
+
+function newScreen() {
+  document.getElementById("body").style.backgroundImage = "";
+}
+
+function hide(n) {
+  if (n = 1) {
+    document.getElementById("codeInput").style.display = "none";
+    document.getElementById("codeInputButton").style.display = "none";
   }
 }
 
